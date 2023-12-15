0433c343254a9563950aaaf0eaa0d462f9ab5233
Cassidy <cassidy.rouelle@gmail.com>
2023-12-15 16:23:31 +0100

    ...

---
+++ 6.Forms/script.js
@@ -0,0 +1,7 @@
+const firstName = document.getElementById('firstname')
+const displayFirstName = document.getElementById('display-firstname')
+const age = document.getElementById('age')
+const hardTrustSection = document.getElementById('a-hard.thruth')
+const password = document.getElementById('pwd')
+const confirmPassword = document.getElementById('pwd-confirm')
+const modeSelect = document.getElementById('toggle-darkmode')
