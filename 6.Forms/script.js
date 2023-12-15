db5ce75c3ac641c60fbedf2c469958e219cfe389
Cassidy <cassidy.rouelle@gmail.com>
2023-12-15 16:25:21 +0100

    ...

--- 6.Forms/script.js
+++ 6.Forms/script.js
@@ -5,3 +5,4 @@ const hardTrustSection = document.getElementById('a-hard.thruth')
 const password = document.getElementById('pwd')
 const confirmPassword = document.getElementById('pwd-confirm')
 const modeSelect = document.getElementById('toggle-darkmode')
+
