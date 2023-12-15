f1256de8e3e99cfefd2ab4ab7ca842b59ea31355
Cassidy <cassidy.rouelle@gmail.com>
2023-12-15 15:29:17 +0100

    ...

--- 5.Events/script.js
+++ 5.Events/script.js
@@ -1,11 +1,25 @@
-
 const _initTime = Date.now()
 
 const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+document.body.addEventListener('keypress', (e) => {
+  if (e.code === 'Space') {
+    document.body.style.backgroundColor = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`
 
+    const actionLog = document.querySelector('ul')
+    const newLog = document.createElement('li')
+    newLog.textContent = `[${getElapsedTime()}] Spacebar was pressed to change background color`
+    actionLog.appendChild(newLog)
+  } else if (e.key === 'l') {
+    const actionLog = document.querySelector('ul')
+    actionLog.innerHTML = ''
+  } else if (e.key === 's'){
+    const displaySquareWrapper = document.querySelector('.displayedsquare-wrapper');
+    displaySquareWrapper.innerHTML = '';
+  }
+})
 
 const clickOnSquare = (e) => {
   /*console.log(e.target.classList[1])
@@ -22,8 +36,11 @@ const clickOnSquare = (e) => {
   const newLog = document.createElement('li')
   newLog.textContent = `[${getElapsedTime()}] Created a new ${color} square`
   actionLog.appendChild(newLog)
-}
 
+  newDisplayedSquare.addEventListener('click', () => {
+    alert(`This square is ${color}`)
+  })
+}
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
